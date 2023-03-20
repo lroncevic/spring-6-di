e406b130b6faeda8377ca901529a332c1c8e760d
lroncevic <roncevicluka@outlook.com>
2023-03-20 21:19:59 +0100

    Added test context example

--- src/test/java/com/lukaroncevic/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/lukaroncevic/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,31 @@
 package com.lukaroncevic.spring6di;
 
+import com.lukaroncevic.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController controller;
+
+	@Test
+	void testAutowireOfController() {
+		System.out.println(controller.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController controller = applicationContext.getBean(MyController.class);
+
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
