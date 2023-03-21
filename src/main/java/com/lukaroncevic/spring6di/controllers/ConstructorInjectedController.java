87411fe1aa02e7fe33c710a853d52db8be71927a
lroncevic <roncevicluka@outlook.com>
2023-03-21 08:54:13 +0100

    Added dependency with Spring

--- src/main/java/com/lukaroncevic/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/com/lukaroncevic/spring6di/controllers/ConstructorInjectedController.java
@@ -1,9 +1,13 @@
 package com.lukaroncevic.spring6di.controllers;
 
 import com.lukaroncevic.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
+    @Autowired
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
